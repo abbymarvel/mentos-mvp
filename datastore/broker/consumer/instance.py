16f5977dfcfed3162862b188f9de8fddf0de2434
rayhan randi <rayhanrandi10@gmail.com>
2024-11-08 00:18:00 +0700

    feat: add impl sql datastore

--- datastore/broker/consumer/instance.py
+++ datastore/broker/consumer/instance.py
@@ -1,17 +1,21 @@
 import json
 
-from config.logging import Logger
-from clickhouse.vectorstore import ClickhouseClient
-
 from kafka import KafkaConsumer
+
+from config.logging import Logger
+from config.utils import get_env_value
+from clickhouse.vectorstore import ClickhouseClient
+from sql.postgresql import PostgreSQLClient
+
 
 
 class Consumer:
-    def __init__(self, broker: str, topic: str, vector_store: ClickhouseClient) -> None:
+    def __init__(self, broker: str, topic: str, vector_store: ClickhouseClient = None, db: PostgreSQLClient = None) -> None:
         self.KAFKA_SERVER = broker
         self.KAFKA_TOPIC = topic
         self.instance = None
         self.vector_store = vector_store
+        self.db = db
         self.logger = Logger().setup_logger(service_name='consumer')
     
     def create_instance(self) -> KafkaConsumer:
@@ -35,7 +39,14 @@ class Consumer:
             for message in self.instance:
                 self.logger.info(f" [*] recv: {message}")
                 # convert data to vector then store data in vector store
-                self.vector_store.write_document(message.value)
+                data = message.value
+                x = data['gyro']
+                y = data['accel']
+                query = f"""
+                    INSERT INTO {get_env_value('TABLE_NAME')} (sensor, time, temp, hum, gyro, accel)
+                    VALUES({data['sensor']}, '{data['time']}', {data['temp']}, '{data['hum']}', '{self.db.to_sql_array(data['gyro'])}', '{self.db.to_sql_array(data['accel'])}')
+                """
+                self.db.query(query)
         except Exception as e:
             self.logger.error(f" [x] Failed to consume message: {e}")
         finally:
--- datastore/main.py
+++ datastore/main.py
@@ -1,6 +1,7 @@
 from broker.consumer.instance import Consumer
 from clickhouse.vectorstore import ClickhouseClient
 from config.utils import setup_env, get_env_value
+from sql.postgresql import PostgreSQLClient
 
 
 if __name__ == '__main__':
@@ -10,26 +11,29 @@ if __name__ == '__main__':
     KAFKA_BROKER = get_env_value('KAFKA_BROKER')
     KAFKA_TOPIC = get_env_value('KAFKA_TOPIC')
 
-    CLICKHOUSE_HOST=get_env_value("CLICKHOUSE_HOST")
-    CLICKHOUSE_PORT=get_env_value("CLICKHOUSE_PORT")
-    CLICKHOUSE_USERNAME=get_env_value("CLICKHOUSE_USERNAME")
-    # CLICKHOUSE_PASSWORD=get_env_value("CLICKHOUSE_PASSWORD")
-    CLICKHOUSE_DATABASE=get_env_value("CLICKHOUSE_DATABASE")
-    CLICKHOUSE_TABLE=get_env_value("CLICKHOUSE_TABLE")
+    # CLICKHOUSE_HOST=get_env_value("CLICKHOUSE_HOST")
+    # CLICKHOUSE_PORT=get_env_value("CLICKHOUSE_PORT")
+    # CLICKHOUSE_USERNAME=get_env_value("CLICKHOUSE_USERNAME")
+    # # CLICKHOUSE_PASSWORD=get_env_value("CLICKHOUSE_PASSWORD")
+    # CLICKHOUSE_DATABASE=get_env_value("CLICKHOUSE_DATABASE")
+    # CLICKHOUSE_TABLE=get_env_value("CLICKHOUSE_TABLE")
 
 
-    clickhouse_client = ClickhouseClient(
-        host=CLICKHOUSE_HOST,
-        port=CLICKHOUSE_PORT,
-        username=CLICKHOUSE_USERNAME,
-        database=CLICKHOUSE_DATABASE,
-        table=CLICKHOUSE_TABLE
-    )
+    # clickhouse_client = ClickhouseClient(
+    #     host=CLICKHOUSE_HOST,
+    #     port=CLICKHOUSE_PORT,
+    #     username=CLICKHOUSE_USERNAME,
+    #     database=CLICKHOUSE_DATABASE,
+    #     table=CLICKHOUSE_TABLE
+    # )
+
+    sql_client = PostgreSQLClient()
 
     consumer = Consumer(
         broker=KAFKA_BROKER, 
         topic=KAFKA_TOPIC,
-        vector_store=clickhouse_client
+        # vector_store=clickhouse_client
+        db=sql_client
     )
 
 
